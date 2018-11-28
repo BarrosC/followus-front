2a530e5c9123c3a1f97376df9d7a15575de01c15
Cristiano Barros <cristiano.barros@zarpsystem.com.br>
2018-11-28 21:02:03 -0200

    Dashboard concluído

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,11 @@
+import { EventosService } from './../eventos/eventos.service';
+import { EventosDetalheDialogComponent } from './../eventos/eventos-detalhe-dialog/eventos-detalhe-dialog.component';
+import { EventoVO } from './../../base/vo/evento';
+import { NoticiasDetalheDialogComponent } from './../noticias/noticias-detalhe-dialog/noticias-detalhe-dialog.component';
+import { NoticiasService } from './../noticias/noticias.service';
+import { MatDialog } from '@angular/material';
 import { Component, OnInit } from '@angular/core';
+import { NoticiaVO } from '../../base/vo/noticia';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +14,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public noticias: Array<NoticiaVO> = [];
+  public eventos: Array<EventoVO> = [];
+  public meusEventos: Array<EventoVO> = [];
 
-  ngOnInit() {    
+  constructor(
+    private dialog: MatDialog,
+    private noticiasService: NoticiasService,
+    private eventosService: EventosService) { }
+
+  ngOnInit() {  
+    this.recuperarNoticias(); 
+    this.recuperarEventos(); 
+  }
+
+  recuperarNoticias() {
+    this.noticiasService.recuperarNoticias().subscribe(response => {
+      this.noticias = response;      
+    },
+    error => {
+      this.noticiasService.hideLoader();
+      this.noticiasService.showError("Erro ao recuperar notícias");
+    });
+  }
+
+  recuperarEventos() {
+    this.eventosService.recuperarEventos().subscribe(response => {
+      this.eventos = response;
+      this.getMyEvents();
+      console.log(response);
+      console.log(this.meusEventos);
+    },
+    error => {
+      this.eventosService.hideLoader();
+      this.eventosService.showError("Erro ao recuperar eventos");
+    });
+  }
+
+  detalheNoticiaDialog(noticia: String) {
+    this.dialog.open(NoticiasDetalheDialogComponent, {
+      data: noticia,
+      disableClose: true
+    });
+  }
+
+  detalheEventoDialog(evento: EventoVO) {
+    this.dialog.open(EventosDetalheDialogComponent, {
+      data: evento,
+      disableClose: true
+    });
+  }
+
+  validateDate(evento: EventoVO) {
+    let eventDate = new Date(evento.dataEvento);
+    return eventDate > new Date();
+  }
+
+  getMyEvents() {
+    const curPersonId = JSON.parse(localStorage.getItem('currentPerson')).id;
+    this.meusEventos = [];
+
+    this.eventos.forEach(evento => {
+      evento.participantes.forEach(participante => {
+        if(participante.personId === curPersonId) {
+          this.meusEventos.push(evento);
+        }
+      });
+    });
+  }
+
+  meusEventosConcluidos() {
+    let count = 0;
+
+    this.meusEventos.forEach(evento => {
+      if(!this.validateDate(evento)) {
+        count ++;
+      }
+    });
+
+    return count;
+  }
+
+  distanciaTotalPercorrida() {
+    let distanciaTotal = 0;
+
+    this.meusEventos.forEach(evento => {
+      if(!this.validateDate(evento)) {
+        distanciaTotal = distanciaTotal + evento.distancia;
+      }
+    });
+
+    return distanciaTotal;
   }
 
 }
